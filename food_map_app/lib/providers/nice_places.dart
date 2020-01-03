9ea62e96a35415ca49a47493c73c6e13b2b49d69
riy0 <chac0ptx@gmail.com>
2020-01-03 19:00:49 +0900

    add/add simple screen

---
+++ food_map_app/lib/providers/nice_places.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import '../models/place.dart';
+
+class GreatPlaces with ChangeNotifier {
+  List<Place> _items = [];
+  List<Place> get items {
+    return [...items];
+  }
+}
